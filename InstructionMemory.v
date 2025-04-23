9e0d5b2941f3a4f63ed6977052196241fe08d78d
Bruno Cassani <113109637+brunocassani@users.noreply.github.com>
2025-04-23 18:15:57 -0400

    add addi and ori instructions

--- InstructionMemory.v
+++ InstructionMemory.v
@@ -14,25 +14,10 @@ module InstructionMemory(input clock,
 				content[i] = 0;
 
 			// Initial values
-			content[0] = 32'h00221820;	// add $3, $1, $2  <- label 'main'
-			content[1] = 32'h00221822;	// sub $3, $1, $2
-			content[2] = 32'h00221824;	// and $3, $1, $2
-			content[3] = 32'h00221825;	// or $3, $1, $2
-			content[4] = 32'h0022182a;	// slt $3, $1, $2
-			content[5] = 32'h0041182a;	// slt $3, $2, $1
-			content[6] = 32'h1140fff9;	// beq $10, $0, main
-			content[7] = 32'h8d430000;	// lw $3, 0($10)
-			content[8] = 32'h8d430004;	// lw $3, 4($10)
-			content[9] = 32'had430008;	// sw $3, 8($10)
-			content[10] = 32'h1000fff5;	// beq $0, $0, main
-
-
-            //ok i can't tell if we're supposed to overwrite the current code
-            //or???
-            //
-            //if we just need to overwrite we can do
-            //content[0] = whatever the hex for (andi $s0, $zero, 0x1) is
-            //do the same for the other 2 instructions           
+			content[0] = 32'h20100001;
+			content[1] = 32'h36108000;	
+			content[2] = 32'h2210ffff;
+        
 		end
 
 	// Read instruction
